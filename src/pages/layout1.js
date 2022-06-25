284efdec96e4e48949f0fd2da7dbed14d923072a
Stephen Parker <sparker888@gmail.com>
2022-06-25 00:45:14 -0400

    added nav to layout1

--- src/pages/layout1.js
+++ src/pages/layout1.js
@@ -1,10 +1,17 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
 const Layout1 = () => {
     return (
+        <>
+        <Header />
         <div className="font-sans font-black text-3xl">
         "Hello, can you hear me now?"
         </div>
+        <Footer />
+        </>
     )
 }
 
